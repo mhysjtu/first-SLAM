7908bb30860a1391c7dced4506b343f509790ffc
mhy-Y7000P <mhysjtu@163.com>
2019-08-12 22:28:49 +0800

    add curve fitting using ceres and g2o

--- README.md
+++ README.md
@@ -1,26 +1,39 @@
 # first-SLAM
 My first SLAM program, learning from gaoxiang12's slambook.
+
 ## Learning Roadmap
 + **Main route:**
 - gao's video -> read gao's code -> **write the code** -> read book carefully and solver the problem -> **do practice**
 + **Side route:**
 - A. For complex parts(e.g. Feature Detection, Epipolar Geometry, etc), read RVC or MVG book to study further.
 - B. For Core parts(e.g. Motion Estimation, Nonlinear Optimization, Loop Detection, etc), read RVCMVG book or ETH course's PPT to practice.
+
 ## 0809 Feature Detection Code
 - in ch7/feature_extraction_orb.cpp
 1. ORB features and descriptors
 2. match: brute force
 3. select matches based on distance
+
 ## 0810 2D-2D pose estimation Code
-- in ch7/pose_estimation_2d2d.cpp
+- in ch7/pose_estimation_2d2d.cpp<br>
+
 1. epipolar geometry 
 - fundamental, essential, homography matrix 
 2. recover pose from essential matrix
 - using cv::recoverPose
+
 ## 0811 2D-3D, 3D-3D pose estimation and triangulation Code
-- in ch7/pose_estimation_2d3d.cpp
+- in ch7/pose_estimation_2d3d.cpp<br>
   using cv::solvePnP(EPnP method)
-- in ch7/pose_estimation_3d3d.cpp
+- in ch7/pose_estimation_3d3d.cpp<br>
   using ICP + SVD(Eigen)
 - in ch7/triangulation.cpp
-  using cv::triangulatePoints
+  using cv::triangulatePoints<br>
+
+## 0812 nonlinear optimization for least square
+- In order to solve 2d3d and 3d3d by BA method, basic nonlinear optimization and ceres/g2o lib should be learned first.
+
+- in ch6/ceres_curve_fitting.cpp<br>
+  using ceres lib to solver curve fitting problem using LM method
+- in ch6/g2o_curve_fitting.cpp<br>
+  using g2o lib to solver curve fitting problem using LM, GN, DogLeg method
---
+++ src/ch6/ceres_curve_fitting.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+
+#include<opencv2/core/core.hpp>//generate random number
+#include<ceres/ceres.h>
+#include<chrono>//timing
+
+using namespace std;
+
+struct CURVE_FITTING_COST
+{
+    CURVE_FITTING_COST(double x, double y):_x(x), _y(y){}
+
+    template<typename T>
+    bool operator()(
+        const T* const abc,
+        T* residual
+    )const{
+        residual[0] = T(_y) - ceres::exp(abc[0]*T(_x)*T(_x) + abc[1]*T(_x) + abc[2]);
+        return true;
+    }
+    const double _x, _y;
+};
+
+
+int main(int argc, char **argv){
+    double a = 1.0, b = 2.0, c = 1.0;
+    int N = 100;
+    double w_sigma = 1.0;
+    cv::RNG rng;
+    double abc[3] = {0, 0, 0};
+
+    vector<double> x_data, y_data;
+
+    //true value plus gaussian noise
+    cout<<"generating data"<<endl;
+    for (int i = 0; i < N; i++)
+    {
+        double x = i/100.0;
+        x_data.push_back(x);
+        y_data.push_back(
+            exp(a*x*x + b*x + c) + rng.gaussian(w_sigma)
+        );
+        cout<<x_data[i]<<" "<<y_data[i]<<endl;
+    }
+
+    //contrust least square problem
+    ceres::Problem problem;
+    for (int i = 0; i < N; i++)
+    {
+        //add residual parts
+        //param1:autoDiffCostFunction(residual struct)
+        //param2:kernel function
+        //param3:params to be estimated
+        problem.AddResidualBlock(
+            new ceres::AutoDiffCostFunction<CURVE_FITTING_COST, 1, 3>(
+                new CURVE_FITTING_COST(x_data[i], y_data[i])
+            ),
+            nullptr,
+            abc
+        );
+    }
+    
+    //configure solver
+    ceres::Solver::Options options;
+    options.linear_solver_type = ceres::DENSE_QR;
+    options.minimizer_progress_to_stdout = true;
+
+    ceres::Solver::Summary summary;
+    
+    chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
+    ceres::Solve(options, &problem, &summary);
+    chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
+
+    chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>>(t2-t1);
+    cout<<"solve time cost = "<<time_used.count()<<" seconds."<<endl;
+
+    cout<<summary.BriefReport()<<endl;
+    cout<<"estimated a, b, c = ";
+    for (auto a:abc) cout<<a<<" ";
+    cout<<endl;
+    
+    return 0;
+
+}
